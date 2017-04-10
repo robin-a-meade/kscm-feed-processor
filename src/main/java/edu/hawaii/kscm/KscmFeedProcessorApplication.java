9002d0fdb7dae653fc8f7fcb2292f6cb8821fd1d
Robin Meade <meader@hawaii.edu>
2017-04-10 11:41:27 -1000

    Initial commit of project generated at start.spring.io
    Parameters used:
    
    Build system:        maven
    Spring Boot version: 1.5.2
    Group:               edu.hawaii
    Artifact:            kscm-feed-processor
    Name:                KSCM feed processor
    Description:         Processes the KSCM kinesis feed
    Package Name:        edu.hawaii.kscm
    Packaging:           Jar
    Java Version:        1.8
    Language:            Java
    Dependencies:        <none specifed>

---
+++ src/main/java/edu/hawaii/kscm/KscmFeedProcessorApplication.java
@@ -0,0 +1,12 @@
+package edu.hawaii.kscm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KscmFeedProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KscmFeedProcessorApplication.class, args);
+	}
+}
